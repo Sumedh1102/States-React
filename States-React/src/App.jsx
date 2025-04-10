15e51797e4e3031a001bab1c9f84e7e66388e45a
SUMEDH1234567 <124816595+Sumedh1102@users.noreply.github.com>
2025-04-10 23:50:09 +0530

    Main

---
+++ States-React/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0); // count starts at 0
+
+  const increase = () => {
+    setCount(count + 1); // update the count
+  };
+
+  const decrease = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+    </div>
+  );
+}
+
+export default Counter;
